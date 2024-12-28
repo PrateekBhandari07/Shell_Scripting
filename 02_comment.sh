1f3594b14f3108f29aeffe8c87026ff05aa09035
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2024-12-28 20:12:19 +0530

    basic scripting

---
+++ 02_comment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "single line comment"
+#this is a single line comment
+#yes this is
+
+
+
+echo "multiline comment"
+<<comment
+this 
+is multiline
+comments
+comment
+~          
